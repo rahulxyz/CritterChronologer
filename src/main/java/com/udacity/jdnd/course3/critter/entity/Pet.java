5aeb43e5fedf0e7ff847c562d0c39f23905e13b4
rahulxyz <rahulguptaxyz15@gmail.com>
2021-04-11 23:28:24 +0530

    entities , db connection, table created

---
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -0,0 +1,70 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private PetType type;
+    private String name;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Customer customer;
+
+    private LocalDate birthDate;
+    private String notes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
@@ -0,0 +1,65 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Schedule {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToMany
+    private List<Employee> employeeIds;
+
+    @ManyToMany
+    private List<Pet> petIds;
+    private LocalDate date;
+
+    @ElementCollection
+    private Set<EmployeeSkill> activities;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployeeIds() {
+        return employeeIds;
+    }
+
+    public void setEmployeeIds(List<Employee> employeeIds) {
+        this.employeeIds = employeeIds;
+    }
+
+    public List<Pet> getPetIds() {
+        return petIds;
+    }
+
+    public void setPetIds(List<Pet> petIds) {
+        this.petIds = petIds;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
